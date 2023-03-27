10161ffdf96dd2b04d559b1da233fef970e8f08e
Javen Wilson <javenb022@gmail.com>
2023-03-27 16:29:08 -0400

    add insert and prepend methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -22,41 +22,63 @@ class LinkedList
     
   end
 
-  def count
-    if empty?
-      0
-    else 
-      count_node(head, 0)
-    end
-  end
-
-  def count_node(node, counter)
-    if node.tail?
-      counter
+  def prepend(data)
+    if @head == nil
+      @head = Node.new(data)
     else
-      count_node(node.next_node, counter +=1)
+      current_node = Node.new(data)
+      current_node.next_node = @head
+      @head = current_node
     end
+    return data
   end
 
+  def insert(index, data)
+    node = Node.new(data)
+    next_node = node_at(head, index)
+    node_at(head, index - 1).next_node = node
+    node.next_node = next_node
+    return node.data
+  end
+
+  def node_at(node, index, counter = 0)
+    if index == counter
+      node_at(node.next_node, index, counter += 1)
+    end
+    return node
+  end
+
+  def count
+    current_node = head
+    if current_node == nil
+      count = 0
+    else
+      count = 1
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        count += 1
+      end
+      count
+    end
+  end
+  
   def to_string
     current_node = head
-    if @head != nil
-      strings = []
-      until current_node.next_node == nil
-        strings << current_node.data
-        current_node = current_node.next_node
-      end
-      strings.join(" ")
-    else
-      
+    # if @head != nil
+    strings = []
+    # while current_node.next_node != nil
+    until current_node == nil
+      strings << current_node.data
+      current_node = current_node.next_node
     end
-    # strings.join(" ")
+    strings.join(" ")
+    # end
   end
-
+  
   def empty?
     head.nil?
   end
-
+  
   def last_node(node)
     if node.tail?
       node
@@ -64,9 +86,24 @@ class LinkedList
       last_node(node.next_node)
     end
   end
-
+  
   def new_node(node)
     Node.new(node)
   end
-
+  
 end
+# def count
+#   if empty?
+#     0
+#   else 
+#     count_node(head, 0)
+#   end
+# end
+
+# def count_node(node, counter)
+#   if node.tail?
+#     counter
+#   else
+#     count_node(node.next_node, counter +=1)
+#   end
+# end
