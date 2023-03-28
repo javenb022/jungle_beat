d630f8615a1c87904e64957041bd67f115975891
Javen Wilson <javenb022@gmail.com>
2023-03-27 23:50:49 -0400

    add tests for jungle beat append and count methods

---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,24 @@
+require "rspec"
+require "./lib/jungle_beat"
+require "./lib/linked_list"
+require "./lib/node"
+
+RSpec.describe JungleBeat do
+  it "exists" do
+    jb = JungleBeat.new
+
+    expect(jb).to be_a(JungleBeat)
+    expect(jb.list.head).to eq(nil)
+  end
+
+  it "Can append, call nodes, and count" do
+    jb = JungleBeat.new
+
+    expect(jb.append("deep doo ditt")).to eq("deep doo ditt")
+    expect(jb.list.head.data).to eq("deep")
+    expect(jb.list.head.next_node.data).to eq("doo")
+    expect(jb.append("woo hoo shu")).to eq("woo hoo shu")
+    expect(jb.count).to eq(6)
+  end
+  
+end
