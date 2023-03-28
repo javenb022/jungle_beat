5b51e38a0da1ffded58de90392926d9ecc4d299b
Javen Wilson <javenb022@gmail.com>
2023-03-27 21:07:22 -0400

    add working tests for insert method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -32,8 +32,8 @@ RSpec.describe LinkedList do
   expect(list.prepend("dop")).to eq("dop")
   expect(list.to_string).to eq("dop plop suu")
   expect(list.count).to eq(3)
-  require 'pry'; binding.pry
   expect(list.insert(1, "woo")).to eq("woo")
-  # expect(list.to_string).to eq("dop woo plop suu")
+  expect(list.to_string).to eq("dop woo plop suu")
+  require 'pry'; binding.pry
  end
 end
